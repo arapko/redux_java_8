8c627ec938fd8a91dc939994f4f721bfe1f95004
vikravch <vitaly.kravchyk@gmail.com>
2021-07-04 21:25:39 +0300

    - add redux, devtool, thunk

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 // Observer - Наблюдатель
 
-import {createStore} from "./createStore";
+//import {createStore} from "./createStore";
 import {countReducer} from "./countReducer";
 import {ADD, SUB} from "./types";
 
-//import {createStore} from "redux";
-
+import {applyMiddleware, createStore} from "redux";
+import {composeWithDevTools} from "redux-devtools-extension";
+import thunk from "redux-thunk";
 const addBtn = document.getElementById('add');
 const subBtn = document.getElementById('sub');
 const counterText = document.getElementById('counter');
 //Ctrl+Shift+Alt+L
 
-const store = createStore(countReducer,0);
+const store = createStore(countReducer,0, composeWithDevTools(applyMiddleware(thunk)));
 
 addBtn.addEventListener('click', () => {
     store.dispatch({type:ADD});
